305263360617f2f59b1ab4fa97c185308bbacc7b
Marco Antonio Luna Serrano <31069393+mluna1001@users.noreply.github.com>
2019-12-30 08:41:52 -0600

    Constantes de iconos de clima

--- weather-app/src/components/WeatherTemperature.js
+++ weather-app/src/components/WeatherTemperature.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import WeatherIcons from 'react-weathericons'
 
+const icons = {
+    sunny: "day-sunny",
+    fog: "day-fog"
+};
+
+const getWeatherIcon = weatherState => {
+    const icon = icons[weatherState];
+    if (icon)
+        return <WeatherIcons name={icon} size="2x" />;
+    else
+        return <WeatherIcons name={"day-sunny"} size="2x" />;
+};
+
 const WeatherTemperature = ({ temperature, weatherState }) => (
 <div>
-    <WeatherIcons name="day-sunny" size="2x" />
+    {
+        getWeatherIcon(weatherState)
+    }
     <span>{`${temperature}°C`}</span>
 </div>
 );
