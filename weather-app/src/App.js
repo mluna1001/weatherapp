55ca2616ea99bbcc2498d630356cdb4277f4d442
Marco Antonio Luna Serrano <mluna1001@gmail.com>
2020-01-22 22:21:56 -0600

    state
    cambio de titulos

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -5,6 +5,7 @@ import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
 import { Grid, Col, Row } from "react-flexbox-grid";
 import LocationList from './components/LocationList';
+import ForecastExtended from "./components/ForecastExtended";
 import './App.css';
 
 const cities = [
@@ -18,17 +19,28 @@ const cities = [
 
 class App extends Component {
 
+  constructor() {
+    super();
+    // Solamente los states se generan en el constructor, en otros lugares hay que utilizar this.SetState()
+    this.state = { city: 'Nueva Ciudad' };
+  }
+
   handleSelectionLocation = city => {
+    this.setState({ city });
     console.log(`handleSelectionLocation ${city}`);
   }
 
   render() {
+
+    // Una buena práctica es usar destructuring para enviar parámetros a componentes
+    const { city } = this.state;
+
     return (
       <Grid>
         <Row>
           <AppBar position="sticky">
             <Toolbar>
-              <Typography variant="title" color="inherit">
+              <Typography variant="subtitle1" color="inherit">
                 Weather App
               </Typography>
             </Toolbar>
@@ -41,8 +53,11 @@ class App extends Component {
             onSelectedLocation={this.handleSelectionLocation}></LocationList>
           </Col>
           <Col xs={12} md={6}>
-            <Paper elevation="4">
-              <div className="details"></div>
+            <Paper elevation={4}>
+              <div className="details">
+                <ForecastExtended city={ city }>
+                </ForecastExtended>
+              </div>
             </Paper>
           </Col>
         </Row>
